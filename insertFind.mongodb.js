9b9ee04fbf7611019287013e17d2fb28e7eee739
taykas <thaynaschaeffer@gmail.com>
2026-02-03 08:45:35 -0300

    iniciando

---
+++ insertFind.mongodb.js
@@ -0,0 +1,56 @@
+// use('db_senai')
+
+// db.people.insertMany([
+//     {
+//         name: 'Thayna',
+//         lastname: 'Schaeffer',
+//         salary: 2
+//     },
+//     {
+//         name: 'Fernanda',
+//         lastname: 'Chata',
+//         salary: 2
+//     },
+//     {
+//         name: 'Joyce',
+//         lastname: 'Vasco',
+//         salary: 2
+//     },
+//     {
+//         name: 'Kessy',
+//         lastname: 'Franca',
+//         salary: 2
+//     },
+//     {
+//         name: 'Malu',
+//         lastname: 'Silva',
+//         salary: 2
+//     },
+//     {
+//         name: 'Jhenie',
+//         lastname: 'Lero',
+//         salary: 2
+//     },
+//     {
+//         name: 'Julia',
+//         lastname: 'Silva',
+//         salary: 2
+//     },
+//     {
+//         name: 'Helo',
+//         lastname: 'Fofa',
+//         salary: 2
+//     },
+//     {
+//         name: 'Ket',
+//         lastname: 'Jomes',
+//         salary: 2
+//     },
+//     {
+//         name: 'Cesar',
+//         lastname: 'Stati',
+//         salary: 2
+//     },
+// ])
+
+db.people.find
