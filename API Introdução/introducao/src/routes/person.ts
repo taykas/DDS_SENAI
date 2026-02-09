df6066714821b339998143be6b6c3df935ca4774
taykas <thaynaschaeffer@gmail.com>
2026-02-09 10:57:24 -0300

    yah

---
+++ API Introdução/introducao/src/routes/person.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response, Router } from 'express';
+
+const router: Router = express.Router();
+const people: object[] = [];
+
+//....
+
+export default router;
--- API Introdução/introducao/src/server.ts
+++ API Introdução/introducao/src/server.ts
@@ -1,35 +1,41 @@
-import express, { response } from 'express'
-import { lstatSync } from 'node:fs';
+import express, { response } from 'express';
+import routes from './routes/routes.ts';
 
 const port = 8080
 const app = express();
 
-const pessoa = {
-    name: 'Thayna',
-    lastname: 'Kaizi'
-}
-
-const pessoas = [
-    {
-        nome: 'teste'
-    },
-    {
-        nome: 'fulano'
-    }
-] 
-
-app.get('/pessoas', (req, res) => {
-    res.send({pessoas: pessoas})
-})
-
-app.get('/objeto', (req, res) => {
-    res.send({pessoa: pessoa})
-})
+app.use(routes)
 
 app.get('/', (req, res) => {
-    res.send({response: 'Api Funcionando'})
+    res.status(200).send({response: 'api funcionando'})
 })
 
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}`)
 });
+
+// const pessoa = {
+//     name: 'Thayna',
+//     lastname: 'Kaizi'
+// }
+
+// const pessoas = [
+//     {
+//         nome: 'teste'
+//     },
+//     {
+//         nome: 'fulano'
+//     }
+// ] 
+
+// app.get('/pessoas', (req, res) => {
+//     res.send({pessoas: pessoas})
+// })
+
+// app.get('/objeto', (req, res) => {
+//     res.send({pessoa: pessoa})
+// })
+
+// app.get('/', (req, res) => {
+//     res.send({response: 'Api Funcionando'})
+// })
