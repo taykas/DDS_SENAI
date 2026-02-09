b2a1a2ff5a02e2f6c4231fda2f8794aec93b29e8
taykas <thaynaschaeffer@gmail.com>
2026-02-09 09:24:14 -0300

    criando objeto na api

--- API Introdução/introducao/src/server.ts
+++ API Introdução/introducao/src/server.ts
@@ -1,7 +1,21 @@
-import express from 'express'
+import express, { response } from 'express'
+import { lstatSync } from 'node:fs';
 
 const port = 8080
 const app = express();
+
+const pessoa = {
+    name: 'Thayna',
+    lastname: 'Kaizi'
+}
+
+app.get('/objeto', (req, res) => {
+    res.send({pessoa: pessoa})
+})
+
+app.get('/', (req, res) => {
+    res.send({response: 'Api Funcionando'})
+})
 
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}`)
