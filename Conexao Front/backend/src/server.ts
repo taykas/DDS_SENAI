df1fd34c03aa7f3adb52cfe45be6558541d75d76
taykas <thaynaschaeffer@gmail.com>
2026-03-09 11:40:21 -0300

    Finishing the day

--- Conexao Front/backend/src/server.ts
+++ Conexao Front/backend/src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import connectDB from './database/database.js';
+import cors from cors;
 
 const app = express();
 const port = 8080;
+
+app.use(cors({
+    origin: '*'
+}));
+
+routes(app);
 
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
 
