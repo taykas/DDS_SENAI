64569fb246b6e33fb102b7a2a469747588d9872f
taykas <thaynaschaeffer@gmail.com>
2026-03-09 10:47:25 -0300

    Revert "Finishing middleware and controllers"
    This reverts commit 3562a80ba1ef8712643ff0dd944646c442be34d3.

--- MongoDB Implementado/introducao middleware/src/models/person.ts
+++ MongoDB Implementado/introducao middleware/src/models/person.ts
@@ -2,14 +2,14 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 interface IPerson extends Document {
     name: string;
-    email: string;
-    password: string;
+    lastname: string;
+    age: number;
 }
 
 const personSchema: Schema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    lastname: { type: String, required: true },
+    age: { type: Number, required: true },
 });
 
 const Person = mongoose.model<IPerson>('Person', personSchema, "Person");
