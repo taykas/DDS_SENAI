7188117726176138881087ee29e2da8f7d29cdbb
taykas <thaynaschaeffer@gmail.com>
2026-03-10 09:06:43 -0300

    att

--- Conexao Front/backend/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import express from 'express'
-import product from './productRoutes.js'
-
-export default function (app: Express) {
-    app
-    .use(express.json())
-    .use('/api/atvMiddleware', product)
-}
